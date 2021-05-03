dfb7fd8d60860f3a59865578c1f3319670a5ec12
Charles de Bueger <chabekah@gmail.com>
2021-05-03 20:19:10 +0100

    Add bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+// https://blog.andreyuhai.com/2021/03/03/how-to-add-bootstrap-to-rails-6
+
+const webpack = require('webpack');
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Rails: '@rails/ujs',
+  })
+);
+
+module.exports = environment;
